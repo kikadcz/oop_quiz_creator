f185dc4501008a28bd5ec0ca33305a4c864798b1
Krizean Dela Cruz <delacruz.krizean.barro@gmail.com>
2025-07-01 14:40:14 +0800

    Added header

--- oop_quiz_creator_project.py
+++ oop_quiz_creator_project.py
@@ -6,3 +6,15 @@ import os
 
 init(autoreset=True)
 
+class QuizCreator:
+    def __init__(self):
+        self.bong_sound = None
+        self.filename = "quiz_questions.txt"
+        self.init_sound()
+
+    def print_header(self):
+        try:
+            ascii_art = pyfiglet.figlet_format("QUIZZATRON 3000", font="doom", justify="left", width=240)
+            print(Fore.RED + "QUIZZATRON 3000")
+            print(f"Error generating ASCII art: {str(e)}")
+
